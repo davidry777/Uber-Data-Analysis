055ef69ef97d81d31b3e5531ce9e50ba5a276be8
davidry777 <dryan0319@gmail.com>
2021-03-23 17:15:29 -0700

    adding more columns to data_2014

--- readUberData.R
+++ readUberData.R
@@ -5,4 +5,19 @@ jul_data <- read.csv(file='Uber-dataset/uber-raw-data-jul14.csv')
 aug_data <- read.csv(file='Uber-dataset/uber-raw-data-aug14.csv')
 sep_data <- read.csv(file='Uber-dataset/uber-raw-data-sep14.csv')
 
-data2014 <- rbind(apr_data, may_data, jun_data, jul_data, aug_data, sep_data)
+data_2014 <- rbind(apr_data, may_data, jun_data, jul_data, aug_data, sep_data)
+
+data_2014$Date.Time <- as.POSIXct(data_2014$Date.Time, format = "%m/%d/%Y %H:%M:%S")
+
+data_2014$Time <- format(as.POSIXct(data_2014$Date.Time, format = "%m/%d/%Y %H:%M:%S"), format="%H:%M:%S")
+
+data_2014$Date.Time <- ymd_hms(data_2014$Date.Time)
+
+data_2014$day <- factor(day(data_2014$Date.Time))
+data_2014$month <- factor(month(data_2014$Date.Time, label = TRUE))
+data_2014$year <- factor(year(data_2014$Date.Time))
+data_2014$dayofweek <- factor(wday(data_2014$Date.Time, label = TRUE))
+
+data_2014$hour <- factor(hour(hms(data_2014$Time)))
+data_2014$minute <- factor(minute(hms(data_2014$Time)))
+data_2014$second <- factor(second(hms(data_2014$Time)))
