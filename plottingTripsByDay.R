2abc25a5c04a7f294d8e07e0d4c33b97f17642c9
davidry777 <dryan0319@gmail.com>
2021-03-23 19:39:26 -0700

    edited visualization code

--- plottingTripsByDay.R
+++ plottingTripsByDay.R
@@ -8,3 +8,13 @@ ggplot(day_group, aes(day, Total)) +
     ggtitle("Trips Every Day") +
       theme(legend.position = "none") +
       scale_y_continuous(labels = comma)
+
+day_month_group <- data_2014 %>%
+  group_by(month, day) %>%
+    dplyr::summarize(Total = n())
+
+ggplot(day_month_group, aes(day, Total, fill = month)) + 
+  geom_bar( stat = "identity") +
+    ggtitle("Trips by Day and Month") +
+      scale_y_continuous(labels = comma) +
+      scale_fill_manual(values = colors)
